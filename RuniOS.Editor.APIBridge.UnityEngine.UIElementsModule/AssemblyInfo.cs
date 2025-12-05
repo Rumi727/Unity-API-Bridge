55c57f6b38b94de5674aa49a4b6c61155b01e342
Rumi <jun32820@gmail.com>
2025-12-05 21:24:07 +0900

    Add Property VisualElementBridge.pseudoStates

--- RuniOS.Editor.APIBridge.UnityEngine.UIElementsModule/AssemblyInfo.cs
+++ RuniOS.Editor.APIBridge.UnityEngine.UIElementsModule/AssemblyInfo.cs
@@ -4,6 +4,7 @@ using UnityEngine.UIElements;
 [assembly: APIBridgeNamespace("RuniOS.Editor.APIBridge")]
 [assembly: GenerateAPIBridgeForAssembly("UnityEngine.UIElementsModule")]
 [assembly: GenerateAPIBridgeForType(typeof(VisualElement), excludeMember = ["m_RunningAnimations", "s_TypeData"], onlyByMyself = true)]
+[assembly: GenerateAPIBridgeForType(typeof(PseudoStates))]
 [assembly: GenerateAPIBridgeForType(typeof(BaseField<>), excludeMember = ["expressionEvaluated"], onlyByMyself = true)]
 [assembly: GenerateAPIBridgeForType(typeof(IPrefixLabel))]
 [assembly: GenerateAPIBridgeForType(typeof(TextInputBaseField<>))]
---
+++ RuniOS.Editor.APIBridge.UnityEngine.UIElementsModule/RuniOS.Editor.APIBridge.UnityEngine.UIElements.VisualElementBridge.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.UIElements;
+
+namespace RuniOS.Editor.APIBridge.UnityEngine.UIElements
+{
+    public partial class VisualElementBridge
+    {
+#pragma warning disable CS1591 // 공개된 형식 또는 멤버에 대한 XML 주석이 없습니다.
+        public PseudoStatesBridge pseudoStates
+        {
+            get => (PseudoStatesBridge)__instance.pseudoStates;
+            set => __instance.pseudoStates = (PseudoStates)value;
+        }
+#pragma warning restore CS1591 // 공개된 형식 또는 멤버에 대한 XML 주석이 없습니다.
+    }
+}
